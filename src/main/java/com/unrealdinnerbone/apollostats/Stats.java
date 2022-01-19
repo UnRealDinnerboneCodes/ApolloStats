a5f0b1d011827f693934403feedd8215fc137b94
unrealdinnerbone@gmail.com <unrealdinnerbone@gmail.com>
2022-01-19 14:13:13 -0600

    Fixed Code

--- src/main/java/com/unrealdinnerbone/apollostats/Stats.java
+++ src/main/java/com/unrealdinnerbone/apollostats/Stats.java
@@ -25,7 +25,7 @@ public class Stats {
     private static final Logger LOGGER = LoggerFactory.getLogger("Stats");
 
     private static final List<IWebPage> generators = new ArrayList<>();
-    private static final Cache<String, String> pages = CacheBuilder.newBuilder().expireAfterWrite(1, TimeUnit.MINUTES).build();
+    private static final Cache<String, String> pages = CacheBuilder.newBuilder().expireAfterWrite(1, TimeUnit.HOURS).build();
 
     static {
 //        generators.add(new GameHostedGen());
@@ -44,14 +44,14 @@ public class Stats {
         Map<String, List<Match>> hostMatchMap = new HashMap<>();
         TaskScheduler.scheduleRepeatingTask(1, TimeUnit.HOURS, () -> {
             try {
-//                Scenarios.updateOfficialScenarios();
+                Scenarios.updateOfficialScenarios();
                 Scenarios.loadDiskData();
                 hostMatchMap.clear();
-//                for(String staff : Util.STAFF) {
-//                    List<Match> matches = getAllMatchesForHost(staff, Optional.empty());
-//                    LOGGER.info("{} has {} matches", staff, matches.stream().filter(Match::isApolloGame).filter(Predicate.not(Match::removed)).count());
-//                    hostMatchMap.put(staff, matches);
-//                }
+                for(String staff : Util.STAFF) {
+                    List<Match> matches = getAllMatchesForHost(staff, Optional.empty());
+                    LOGGER.info("{} has {} matches", staff, matches.stream().filter(Match::isApolloGame).filter(Predicate.not(Match::removed)).count());
+                    hostMatchMap.put(staff, matches);
+                }
             }catch(Exception e) {
                 LOGGER.error("Error while updating data", e);
             }
