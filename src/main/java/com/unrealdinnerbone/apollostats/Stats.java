ccdacce6f3bf784ee4abc044a488241585db4b2e
unrealdinnerbone@gmail.com <unrealdinnerbone@gmail.com>
2022-01-19 15:32:24 -0600

    one last time

--- src/main/java/com/unrealdinnerbone/apollostats/Stats.java
+++ src/main/java/com/unrealdinnerbone/apollostats/Stats.java
@@ -75,7 +75,6 @@ public class Stats {
     public static List<Match> getAllMatchesForHost(String name, Optional<Integer> before) throws Exception {
         String json = HttpUtils.get("https://hosts.uhc.gg/api/hosts/" + name.replace(" ", "%20/") + "/matches?count=50" + before.map(i -> "&before=" + i).orElse("")).body();
         try {
-            LOGGER.info("JSon: {}", json);
             List<Match> matches = Arrays.stream(Util.parser().parse(Match[].class, json)).collect(Collectors.toList());
             if (matches.size() == 50) {
                 matches.addAll(getAllMatchesForHost(name, Optional.of(matches.get(49).id())));
